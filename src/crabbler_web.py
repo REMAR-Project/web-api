821550800d0f2b73126efedd1ae8d0203fd5d1e7
Simon Wells <siwells@gmail.com>
2017-01-17 09:34:56 +0000

    Add logging setup

--- src/crabbler_web.py
+++ src/crabbler_web.py
@@ -1,11 +1,26 @@
-from flask import Flask
+import logging
+
+from logging.handlers import RotatingFileHandler
+from flask import Flask, url_for
 app = Flask(__name__)
 
+def logs(app):
+    log_pathname = 'var/crabbler_web.log'
+    file_handler = RotatingFileHandler(log_pathname, maxBytes=1024* 1024 * 10 , backupCount=1024)
+    file_handler.setLevel( app.config['DEBUG'] )
+    formatter = logging.Formatter("%(levelname)s | %(asctime)s |  %(module)s | %(funcName)s | %(message)s")
+    file_handler.setFormatter(formatter)
+    app.logger.setLevel('DEBUG')
+    app.logger.addHandler(file_handler)
+
 @app.route("/")
-def hello():
+def root():
+    this_route = url_for('.root')
+    app.logger.info("Logging a test message from "+this_route)
     return "Hello Crabs!"
 
 
 if __name__ == "__main__":
+    logs(app)
     app.run(host="0.0.0.0", debug=True)
 
