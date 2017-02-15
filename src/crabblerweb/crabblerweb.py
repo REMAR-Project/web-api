0ed2b7ab59be425d1fbb1f01b9006fbfabcfc146
Simon Wells <siwells@gmail.com>
2017-02-15 18:56:52 +0000

    & another attempt

--- src/crabblerweb/crabblerweb.py
+++ src/crabblerweb/crabblerweb.py
@@ -11,6 +11,18 @@ app.config['DEBUG'] = True
 app.config['threaded'] = True
 app.config['ip_address'] = '0.0.0.0'
 
+import logging
+from logging.handlers import RotatingFileHandler
+
+log_pathname = 'var/crabbler_web.log'
+file_handler = RotatingFileHandler(log_pathname, maxBytes=1024* 1024 * 10 , backupCount=1024)
+file_handler.setLevel( app.config['DEBUG'] )
+formatter = logging.Formatter("%(levelname)s | %(asctime)s |  %(module)s | %(funcName)s | %(message)s")
+file_handler.setFormatter(formatter)
+app.logger.setLevel('DEBUG')
+app.logger.addHandler(file_handler)
+
+
 @app.route("/")
 def root():
     this_route = url_for('.root')
@@ -95,6 +107,5 @@ def up():
 
 
 if __name__ == "__main__":
-    configuration.logs(app)
     app.run(host="0.0.0.0", debug=True)
 
