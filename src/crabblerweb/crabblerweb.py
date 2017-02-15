855f0b35bc21577eed2dea31b7d092d0329a4b19
Simon Wells <siwells@gmail.com>
2017-02-15 17:26:15 +0000

    simplify users route

--- src/crabblerweb/crabblerweb.py
+++ src/crabblerweb/crabblerweb.py
@@ -44,22 +44,16 @@ def legacy_api_auth():
 
 #    Client supplies JSON doc containing phone_id key. Server responds
 #    with JSON doc containing a phone_id key and unique token value
-@app.route("/api/0.2/users", methods=['POST', 'GET'])
+@app.route("/api/0.2/users", methods=['POST'])
 def legacy_api_users():
     msg = "Legacy users API"
     status = "ok"
     statuscode = 200
 
-    if request.method == 'POST':
-        json_data = request.json
-        app.logger.info(json.dumps(json_data))
+    json_data = request.json
+    app.logger.info(json.dumps(json_data))
         
-        return jsonify( {'status':status, 'statuscode':statuscode, 'phone_id':'phoneid-1234abcd'} ), statuscode
-
-    if 'text/html' in request.headers.get("Accept", ""):
-        return Markup(msg), statuscode
-    else:
-        return jsonify( {'status':status, 'statuscode':statuscode, 'message':msg} ), statuscode
+    return jsonify( {'status':status, 'statuscode':statuscode, 'phone_id':'phoneid-1234abcd'} ), statuscode
 
 
 @app.route("/api/0.2/sightings", methods=['POST', 'GET'])
